4683db201b7bb485bf859209cc38cb9dd19a47d7
Vikram Dighe <vdighe@mag.(none)>
2009-06-20 03:31:00 -0400

    Specified join table, since we kind of named it backwards.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   belongs_to :category
   belongs_to :author
   has_many :commments
-  has_and_belongs_to_many :tags
+  has_and_belongs_to_many :tags, {:join_table => :tags_posts}
 end
